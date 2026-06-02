cbc4411ebf7faa26d37982b498012deab7405306
Viola Krasniqi <vividapro200@gmail.com>
2026-06-02 15:16:07 +0200

    Update pythonService.js

--- services/pythonService.js
+++ services/pythonService.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 const computeSimilarity = async (text1, text2, model = "minilm") => {
   const response = await axios.post(
-    "http://python-service/compute-embeddings",
+    "https://ai-text-similarity-python.onrender.com/compute-embeddings",
     {
       text1,
       text2,
